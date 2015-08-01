3897b975050b58cea08769ead39404558852a952
Joe Francis <joe@lostapathy.com>
2015-07-31 20:46:58 -0500

    better docs

--- lib/neurio/reading.rb
+++ lib/neurio/reading.rb
@@ -1,39 +1,18 @@
 class Neurio
+  # A single Neurio sensor reading
+  #
+  # @attr_reader consumption_power [FixNum] The instantaneous power consumption, in watts
+  # @attr_reader consumption_energy [FixNum] The cumulative power consumption since the sensor was activated, in joules
+  # @attr_reader timestamp [Time] The timestamp of the sensor reading
+  # @attr_reader sensor_id [String] The Neurio sensorId of the sensor
   class Reading
+    attr_accessor :consumption_power, :timestamp, :consumption_energy, :sensor_id
+    
     def initialize(values, sensor_id)
       @timestamp = DateTime.parse(values["timestamp"]).to_time
       @consumption_power = values["consumptionPower"]
       @consumption_energy = values["consumptionEnergy"]
       @sensor_id = sensor_id
     end
-    
-    # The instantaneous power consumption, in watts
-    #
-    # @return [FixNum] power consumption
-    def consumption_power
-      @consumption_power
-    end
-    
-    # The timestamp of the sensor reading
-    #
-    # @return [Time] reading timestamp
-    def timestamp
-      @timestamp
-    end
-    
-    # The cumulative power consumption since the sensor was activated, in joules
-    #
-    # @return [FixNum] Total energy consumed
-    def consumption_energy
-      @consumption_energy
-    end
-    
-    # The Neurio sensorId for the sensor
-    #
-    # @return [String] id of the sensor
-    def sensor_id
-      @sendor_id
-    end
-    
   end
 end
